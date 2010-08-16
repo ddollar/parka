112c103e524e236f2b1039581754fca1e2b8975c
David Dollar <ddollar@gmail.com>
2010-08-16 02:41:08 -0400

    add development deps

--- lib/parka/specification.rb
+++ lib/parka/specification.rb
@@ -7,11 +7,14 @@ class Parka::Specification < Gem::Specification
   def self.new(&block)
     spec = Gem::Specification.new(&block)
 
+    # set up some sensible defaults
     spec.author ||= default_author
     spec.email  ||= default_email
 
+    # default file list if none specified
     spec.files = default_files if spec.files.empty?
 
+    # get dependencies from bundler
     add_bundler_dependencies(spec)
 
     spec
@@ -32,9 +35,14 @@ private ######################################################################
   end
 
   def self.add_bundler_dependencies(spec)
-    groups = Bundler.definition.groups - [:development, :test]
-    deps   = Bundler.definition.dependencies.select { |d| (d.groups & groups).any? }
-    deps.each { |d| spec.add_dependency(d.name, d.requirement) }
+    groups  = Bundler.definition.groups - [:development, :test]
+    deps    = Bundler.definition.dependencies
+
+    runtime     = deps.select { |d| (d.groups & groups).any? }
+    development = deps - runtime
+
+    development.each { |d| spec.add_development_dependency(d.name, d.requirement) }
+    runtime.each     { |d| spec.add_dependency(d.name, d.requirement) }
   end
 
 end
