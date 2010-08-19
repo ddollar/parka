dafc62e4dfd04094496ddd4854fb2d64a32b550e
David Dollar <ddollar@gmail.com>
2010-08-19 17:27:42 -0400

    github integration

--- lib/parka/cli.rb
+++ lib/parka/cli.rb
@@ -1,6 +1,7 @@
 require "rubygems/commands/push_command"
 require "rubygems/dependency_installer"
 require "parka"
+require "rest-client"
 require "thor"
 
 class Parka::CLI < Thor
@@ -32,7 +33,32 @@ class Parka::CLI < Thor
   desc "push [GEMSPEC]", "Build the gem and push it to GitHub and RubyGems.org"
 
   def push(gemspec_filename=nil)
+    gemspec = Gem::Specification.load(gemspec_filename || default_gemspec)
     gemfile = build(gemspec_filename)
+
+    # create github remote
+    remotes = %x{ git remote }.strip.split("\n")
+    unless remotes.include?("github")
+      %x{ git remote add github git@github.com:#{github_username}/#{gemspec.name}.git }
+    end
+
+    # create github repo
+    repos = github_repositories
+    names = repos.map { |repo| repo["name"] }
+    unless names.include?(gemspec.name)
+      github["repos/create"].post(
+        :name        => gemspec.name,
+        :description => gemspec.summary,
+        :homepage    => gemspec.homepage,
+        :public      => "1"
+      )
+    end
+
+    # create release tag
+    %x{ git tag v#{gemspec.version} }
+
+    # push to github
+    %x{ git push github master --tags }
 
     # push to rubygems
     pusher = Gem::Commands::PushCommand.new
@@ -54,6 +80,24 @@ private ######################################################################
   def error(message)
     puts "ERROR: #{message}"
     exit 1
+  end
+
+  def github
+    username = github_username + "/token"
+    password = github_token
+    RestClient::Resource.new("http://github.com/api/v2/json", username, password)
+  end
+
+  def github_username
+    %x{ git config github.user }.strip
+  end
+
+  def github_token
+    %x{ git config github.token }.strip
+  end
+
+  def github_repositories
+    Crack::JSON.parse(github["repos/show/#{github_username}"].get)["repositories"]
   end
 
 end
